d3266bcdf697deb01319d38532cd6fd969d80592
Dirk Levinus Nicolaas <dirkncl@gmail.com>
2024-07-15 19:25:51 +0800

    Add files via upload

---
+++ node_modules/codemirror-6.65.7/src/codemirror.js
@@ -0,0 +1,5 @@
+import { CodeMirror } from "./edit/main.js"
+
+globalThis.CodeMirror = CodeMirror
+
+export default CodeMirror
---
+++ node_modules/codemirror-6.65.7/src/util/insert-css.js
@@ -0,0 +1,24 @@
+﻿export function insertCSSStyleSheet(cssStyleSheet) {
+  var rules = [];
+  for(var key of cssStyleSheet.rules) {
+    rules.push(key.cssText)
+  }
+  var css = rules.join('\n');
+  var sty = document.createElement('style')
+  sty.textContent = css
+  document.head.appendChild(sty)
+}
+
+export function insertCSSStyleLink(cssStyleSheet) {
+  var rules = [];
+  for(var key of cssStyleSheet.rules) {
+    rules.push(key.cssText)
+  }
+  var css = rules.join('\n');
+  var blob = new Blob([css], { type: "text/css" }); 
+  var link = document.createElement('link')
+  link.type = "text/css"
+  link.rel = 'stylesheet'
+  link.href = URL.createObjectURL(blob); 
+  document.head.appendChild(link)
+}
